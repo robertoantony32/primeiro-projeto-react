bf19350d33af823be515b5480fad5a4f218dd7e6
robertoantony32 <ragdsf.snf20@uea.edu.br>
2024-07-07 21:22:55 -0400

    hotfix: ajuste em um erro de espaçamento.

--- src/components/Title.js
+++ src/components/Title.js
@@ -1,39 +1,38 @@
 import React, { useEffect, useRef } from "react";
 
 function splitTextIntoSpans(element) {
+  if (!element) return;
+  element.classList.add("split-text");
+  let text = element.innerText;
+  let splitText = text
+    .split(' ')
+    .map(function (word) {
+      let char = word
+        .split('')
+        .map((char) => {
+          return `<span class="split-char">${char}</span>`;
+        })
+        .join('');
 
-    if (!element) return;
+      return `<div class="split-word">${char}</div>`;
+    })
+    .join('');
 
-
-    element.classList.add("split-text");
-    let text = element.innerText;
-    let splitText = text
-      .split(" ")
-      .map(function (word) {
-        let char = word
-          .split("")
-          .map((char) => {
-            return `<span class="split-char">${char}</span>`;
-          })
-          .join("");
-
-        return `<div class="split-word">${char}&nbsp</div>`;
-      })
-      .join("");
-
-    element.innerHTML = splitText;
+  element.innerHTML = splitText;
 }
 
-
 const Title = () => {
-    const titleRef = useRef(null);
+  const titleRef = useRef(null);
 
-    useEffect(() => {
-        splitTextIntoSpans(titleRef.current);
-    }, []);
+  useEffect(() => {
+    splitTextIntoSpans(titleRef.current);
+  }, []);
 
-    return  <div ref={titleRef} className="title">Localizar CEP</div>
+  return (
+    <div ref={titleRef} className="title">
+      Localizar CEP
+    </div>
+  );
 };
-
 
 export default Title;
